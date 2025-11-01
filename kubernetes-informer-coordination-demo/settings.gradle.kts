de8c5575ebfc86f3ad9f2e97ae927867f791135e
rmpader <reynaldmpader@gmail.com>
2025-11-01 18:00:50 +0800

    Rename kubernetes-informer-demo

--- kubernetes-informer-coordination-demo/settings.gradle.kts
+++ kubernetes-informer-coordination-demo/settings.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
 }
-rootProject.name = "kubernetes-informer-demo"
+rootProject.name = "kubernetes-informer-coordination-demo"
 includeBuild("../../kotlin-event-sourcing")
